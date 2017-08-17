97b0a59dcf9b842f3a50548e4cfe70fd2fd978a4
taylor <Liang.tang@cootek.cn>
2017-08-17 22:13:41 +0800

    [feat] switch image when clicking

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/SelectorActivity.java
@@ -0,0 +1,21 @@
+package test.taylor.com.taylorcode.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import test.taylor.com.taylorcode.R;
+
+/**
+ * Created on 17/8/17.
+ */
+
+public class SelectorActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selector_activity);
+    }
+}
