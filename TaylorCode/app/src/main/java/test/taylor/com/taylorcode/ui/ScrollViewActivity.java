b2f5eae3b8e1c5a6a96f1afce89b6c21f149224a
taylor <Liang.tang@cootek.cn>
2017-08-20 19:44:04 +0800

    [feat] wrap RelativeLayout with ScrollView for acquiring scrolling ability

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/ScrollViewActivity.java
@@ -0,0 +1,21 @@
+package test.taylor.com.taylorcode.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import test.taylor.com.taylorcode.R;
+
+/**
+ * Created on 17/8/20.
+ */
+
+public class ScrollViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //case1:wrap ScrollView with RelativeLayout for add scrolling ability
+        setContentView(R.layout.scrollview_activity);
+    }
+}
