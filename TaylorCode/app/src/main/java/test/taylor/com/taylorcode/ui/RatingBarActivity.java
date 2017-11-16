1f187c436298df46560834c6f53d2a5809339a8c
taylor <liang.tang@cootek.cn>
2017-11-16 14:16:57 +0800

    [feat] add rating listener to rating bar

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/RatingBarActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/RatingBarActivity.java
@@ -3,6 +3,7 @@ package test.taylor.com.taylorcode.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.RatingBar;
 
 import test.taylor.com.taylorcode.R;
@@ -13,8 +14,15 @@ import test.taylor.com.taylorcode.R;
 
 public class RatingBarActivity extends Activity {
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rating_activity);
+        com.hedgehog.ratingbar.RatingBar ratingBar = ((com.hedgehog.ratingbar.RatingBar) findViewById(R.id.rating_bar)) ;
+        ratingBar.setOnRatingChangeListener(new com.hedgehog.ratingbar.RatingBar.OnRatingChangeListener() {
+            @Override
+            public void onRatingChange(float RatingCount) {
+                Log.v("ttaylor", "RatingBarActivity.onRatingChange(): rating="+RatingCount);
+            }
+        });
     }
 }
