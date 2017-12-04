8f1b84acdbb69f81a26870e24980342fc6edd870
taylor <liang.tang@cootek.cn>
2017-12-04 17:35:47 +0800

    [feat] initialization of architecture component

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/architecture_components/MyViewModel.java
@@ -0,0 +1,13 @@
+package test.taylor.com.taylorcode.architecture_components;
+
+import android.arch.lifecycle.ComputableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * Created on 2017/11/30.
+ */
+
+public class MyViewModel extends ViewModel {
+
+    private ComputableLiveData<String> liveData ;
+}
