42a5acebe48491831c77fceda42c5816a138fff0
taylor <liang.tang@cootek.cn>
2018-03-06 16:35:13 +0800

    [feat]activity launch mode :FLAG_ACTIVITY_TASK_ON_HOME

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/launch_mode/LaunchModeActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/launch_mode/LaunchModeActivity.java
@@ -43,21 +43,28 @@ public class LaunchModeActivity extends ActivityBase {
      * launch mode case3:FLAG_ACTIVITY_NEW_TASK
      * start an new activity with an different task affinity and FLAG_ACTIVITY_NEW_TASK,then the new activity will in new task
      * the new activity will be in current task if task affinity not set
+     *
+     * launch mode case15:FLAG_ACTIVITY_TASK_ON_HOME
+     * start an activity and let it related to launcher task,it means when navigate back from the activity launcher will show up(not the task which starts the )
      */
     @Override
     public void onButton2Click() {
         super.onButton2Click();
-        startActivity(ActivityD.class, Intent.FLAG_ACTIVITY_NEW_TASK, null);
+//        startActivity(ActivityD.class, Intent.FLAG_ACTIVITY_NEW_TASK, null);
+        startActivity(ActivityD.class, Intent.FLAG_ACTIVITY_TASK_ON_HOME | Intent.FLAG_ACTIVITY_NEW_TASK, null);//case15
     }
 
     /**
      * launch mode case8:SINGLE_INSTANCE
      * start a new activity with singleInstance mode,and start another activity with standard mode
+     * a new task will be created only for activityG,
+     * no other activities is allowed to be in this task,
+     * even if the activity has the same task affinity with activityG,in this case,a new task whose task affinity is the same as activityG will be create
      */
     @Override
     public void onButton3Click() {
         super.onButton3Click();
-        startActivity(ActivityG.class,null, null);
+        startActivity(ActivityG.class, null, null);
     }
 
     /**
@@ -67,6 +74,6 @@ public class LaunchModeActivity extends ActivityBase {
     @Override
     public void onButton4Click() {
         super.onButton4Click();
-        startActivity(ActivityI.class,null,null);
+        startActivity(ActivityI.class, null, null);
     }
 }
