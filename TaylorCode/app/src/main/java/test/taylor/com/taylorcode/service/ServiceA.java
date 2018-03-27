988906d3deb56cf4ad13ae426cb6f38327c935a9
taylor <liang.tang@cootek.cn>
2018-03-27 15:03:22 +0800

    [feat]stop service

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/service/ServiceA.java
@@ -0,0 +1,33 @@
+package test.taylor.com.taylorcode.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created on 2018/3/23.
+ */
+
+public class ServiceA extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v("ttaylor", "ServiceA.onCreate(): ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("ttaylor", "ServiceA.onDestroy(): ");
+    }
+
+
+}
---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/service/ServiceActivity.java
@@ -0,0 +1,38 @@
+package test.taylor.com.taylorcode.service;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+import test.taylor.com.taylorcode.R;
+
+/**
+ * Created on 2018/3/23.
+ */
+
+public class ServiceActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_activity);
+        findViewById(R.id.btn_stop_service).setOnClickListener(this);
+
+        Intent intent = new Intent(this, ServiceA.class);
+        startService(intent);
+    }
+
+    /**
+     * service case1:stop service by stopService()
+     * this method could stop the service even if the it is in the other process
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        Log.v("ttaylor", "ServiceActivity.onClick(): ");
+        stopService(new Intent(this, ServiceA.class));
+    }
+}
