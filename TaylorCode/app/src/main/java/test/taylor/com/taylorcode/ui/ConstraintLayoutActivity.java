d4826d12a5015ecdc6edc2721826c569729d0b72
taylor <liang.tang@cootek.cn>
2018-05-03 11:48:04 +0800

    [feat] constraintLayout chain

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/ConstraintLayoutActivity.java
@@ -0,0 +1,16 @@
+package test.taylor.com.taylorcode.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import test.taylor.com.taylorcode.R;
+
+public class ConstraintLayoutActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout_activity);
+    }
+}
