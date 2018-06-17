24a3ed8c254362a30317b4c784a1fcf41a56e6f3
taylor <liang.tang@cootek.cn>
2018-06-17 09:47:24 +0800

    [feat] java quote test

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/TaylorApplication.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/TaylorApplication.java
@@ -1,6 +1,9 @@
 package test.taylor.com.taylorcode;
 
 import android.app.Application;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 import test.taylor.com.taylorcode.proxy.system.ActivityHook;
 import test.taylor.com.taylorcode.proxy.system.ClipboardHook;
@@ -15,5 +18,21 @@ public class TaylorApplication extends Application {
         super.onCreate();
 //        ClipboardHook.getInstance().init(this);
 //        ActivityHook.getInstance().init(HookSystemServiceActivity.class);
+
+
+        //java quote case1:
+        ArrayList<String> origin = new ArrayList();
+        origin.add("a");
+        origin.add("b");
+        origin.add("c");
+        makeChange(origin) ;
+        for (String str :
+                origin) {
+            Log.v("ttaylor", "TaylorApplication.onCreate()" + "  str="+str);
+        }
+    }
+
+    private void makeChange(ArrayList<String> origin) {
+        origin.remove(0) ;
     }
 }
