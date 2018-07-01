da9e46482f17e9a6f68e2d15e5f1ebc33eb5d81a
taylor <liang.tang@cootek.cn>
2018-07-01 21:20:29 +0800

    [feat]customize PopupWindow

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/window/CustomPopupWindow.java
@@ -0,0 +1,265 @@
+package test.taylor.com.taylorcode.ui.window;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+
+/**
+ * window case3:customize PopupWindow
+ * the PopupWindow which could be customized by the following aspect:
+ * 1. the content of window which is a ViewGroup as a param of constructor
+ * 2. the click listener of content's children
+ * 3. the animation of showing and dismissing, including duration ,interpolater
+ */
+public class CustomPopupWindow extends PopupWindow implements View.OnTouchListener {
+
+    private float dimAlpha = 0.5f;
+    private int animateDuration = 300;
+    private Activity hostActivity;
+    private ViewGroup windowView;
+    private int gravity;
+    private Rect mTouchFrame;
+    private OnItemClickListener onItemClickListener;
+    protected GestureDetector gestureDetector;
+
+    public CustomPopupWindow(ViewGroup windowView, int width, int height, @NonNull Activity hostActivity) {
+        super(windowView, width, height);
+        this.windowView = windowView;
+        this.hostActivity = hostActivity;
+        setTouchInterceptor(windowView);
+    }
+
+    private void setTouchInterceptor(ViewGroup windowView) {
+        if (windowView == null) {
+            Log.v("ttaylor", "CustomPopupWindow.setOnTouchListener()" + "  window view is null");
+            return;
+        }
+        gestureDetector = new GestureDetector(windowView.getContext(), new GestureListener(windowView));
+        this.setTouchInterceptor(this);
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @Override
+    public void showAtLocation(View parent, int gravity, int x, int y) {
+        super.showAtLocation(parent, gravity, x, y);
+        this.gravity = gravity;
+        prepareShowAnimation(windowView);
+        changeBackgroundAlpha(hostActivity, dimAlpha);
+    }
+
+    /**
+     * prepare the showing animation
+     *
+     * @param windowView
+     */
+    private void prepareShowAnimation(final View windowView) {
+        if (windowView == null) {
+            return;
+        }
+        switch (gravity) {
+            case Gravity.BOTTOM:
+                animateFromBottom(windowView, animateDuration);
+                break;
+        }
+    }
+
+    /**
+     * make this window animate from the bottom of screen to show up
+     *
+     * @param windowView
+     * @param animateDuration
+     */
+    private void animateFromBottom(final View windowView, final int animateDuration) {
+        //get the real height of window view
+        windowView.post(new Runnable() {
+            @Override
+            public void run() {
+                windowView.setTranslationY(windowView.getMeasuredHeight());
+            }
+        });
+        //animate the window view
+        windowView.post(new Runnable() {
+            @Override
+            public void run() {
+                ViewCompat.animate(windowView)
+                        .translationY(0)
+                        .setInterpolator(new LinearOutSlowInInterpolator())
+                        .setDuration(animateDuration)
+                        .start();
+            }
+        });
+    }
+
+    @Override
+    public void dismiss() {
+        //1.do the dismiss animation firstly
+        windowView.post(new Runnable() {
+            @Override
+            public void run() {
+                animateToBottom(windowView, animateDuration);
+            }
+        });
+
+        //2.do the real dismiss secondly, or dismiss animation wont show
+        windowView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                CustomPopupWindow.super.dismiss();
+            }
+        }, animateDuration);
+
+        changeBackgroundAlpha(hostActivity, 1.0f);
+    }
+
+    /**
+     * make this window animate back to screen bottom to dismiss,this is the reverse animation of animateFromBottom()
+     *
+     * @param windowView
+     * @param animateDuration
+     */
+    private void animateToBottom(final View windowView, int animateDuration) {
+        ViewCompat.animate(windowView)
+                .translationY(windowView.getMeasuredHeight())
+                .setInterpolator(new LinearOutSlowInInterpolator())
+                .setDuration(this.animateDuration)
+                .start();
+    }
+
+    /**
+     * dim or light the background of screen when this window show or dismiss
+     *
+     * @param activity
+     * @param alpha
+     */
+    private void changeBackgroundAlpha(Activity activity, float alpha) {
+        if (activity == null) {
+            Log.v("ttaylor", "CustomPopupWindow.changeBackgroundAlpha()" + "  host activity is null");
+            return;
+        }
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.alpha = alpha;
+        window.setAttributes(layoutParams);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int x = (int) event.getX();
+        final int y = (int) event.getY();
+
+        //dismiss this window when clicking outside of itself
+        if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+            dismiss();
+            return true;
+        } else {
+            return gestureDetector.onTouchEvent(event);
+        }
+    }
+
+    /**
+     * this method make it possible that clicking event is implemented outside of PopupWindow
+     */
+    public interface OnItemClickListener {
+        void onWindowItemClick(View view);
+    }
+
+    /**
+     * convert touch event into click event
+     */
+    private class GestureListener implements GestureDetector.OnGestureListener {
+
+        private static final int INVALID_POSITION = -1;
+        private ViewGroup viewGroup;
+
+        public GestureListener(ViewGroup viewGroup) {
+            super();
+            this.viewGroup = viewGroup;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Log.v("ttaylor", "GestureListener.onSingleTapUp()" + "  event=" + e);
+            int x = (int) e.getX();
+            int y = (int) e.getY();
+            int position = pointToPosition(viewGroup, x, y);
+            Log.v("ttaylor", "GestureListener.onSingleTapUp()" + "  click position=" + position);
+            if (position != INVALID_POSITION) {
+                View item = viewGroup.getChildAt(position);
+                if (onItemClickListener != null) {
+                    onItemClickListener.onWindowItemClick(item);
+                    dismiss();
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return false;
+        }
+
+        /**
+         * convert touch point into the position of window's children
+         * @param viewGroup
+         * @param x
+         * @param y
+         * @return
+         */
+        public int pointToPosition(ViewGroup viewGroup, int x, int y) {
+            if (viewGroup == null) {
+                return INVALID_POSITION;
+            }
+            Rect frame = mTouchFrame;
+            if (frame == null) {
+                mTouchFrame = new Rect();
+                frame = mTouchFrame;
+            }
+
+            final int count = viewGroup.getChildCount();
+            for (int i = count - 1; i >= 0; i--) {
+                final View child = viewGroup.getChildAt(i);
+                if (child.getVisibility() == View.VISIBLE) {
+                    child.getHitRect(frame);
+                    if (frame.contains(x, y)) {
+                        return i;
+                    }
+                }
+            }
+            return INVALID_POSITION;
+        }
+    }
+}
+
---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/window/WindowActivity.java
@@ -0,0 +1,204 @@
+package test.taylor.com.taylorcode.ui.window;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import test.taylor.com.taylorcode.R;
+
+public class WindowActivity extends Activity implements View.OnClickListener, CustomPopupWindow.OnItemClickListener {
+
+    //    private PopupWindow popupWindow;
+    private CustomPopupWindow popupWindow;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View content = LayoutInflater.from(this).inflate(R.layout.window_activity, null);
+        setContentView(content);
+        findViewById(R.id.btn_show_window).setOnClickListener(this);
+        findViewById(R.id.btn_show_popup_window).setOnClickListener(this);
+    }
+
+    private View getWindowView(Context context) {
+        View view = LayoutInflater.from(context).inflate(R.layout.window_content, null);
+        return view;
+    }
+
+
+    /**
+     * window case1:show window
+     * TBC: how to dismiss
+     *
+     * @param context
+     */
+    private void showWindow(Context context) {
+        if (context == null) {
+            Log.v("ttaylor", "WindowActivity.showWindow()" + "  context is null");
+            return;
+        }
+        WindowManager windowManager = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE));
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        //this flag allow touching outside window
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        //android.permission.SYSTEM_ALERT_WINDOW is needed,or permission denied for window type 2003
+        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.gravity = Gravity.CENTER;
+        windowManager.addView(getWindowView(context), layoutParams);
+    }
+
+    //transition is much more customizable than setAnimationStyle() but there is api level constraint
+//    private Transition getTransition(int gravity){
+//        Slide slide = new Slide(gravity) ;
+//        slide.setDuration(800) ;
+//        return slide ;
+//    }
+
+
+    /**
+     * window case2:popup window shown at the bottom of screen
+     * TBC:how to use interpolation
+     *
+     * @param context
+     * @param rootView the root view of the hole activity
+     */
+    public void showBottomPopupWindow(Context context, View rootView) {
+//        if (popupWindow == null) {
+//            View windowView = getWindowView(context);
+//            bindWindowClickListener(windowView);
+//            popupWindow = new PopupWindow(windowView,
+//                    ViewGroup.LayoutParams.MATCH_PARENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT, true);
+//
+//            popupWindow.setBackgroundDrawable(new BitmapDrawable());
+//            //TBC:interceptor touch event on popupwindow
+//            popupWindow.setTouchInterceptor(new View.OnTouchListener() {
+//                @Override
+//                public boolean onTouch(View v, MotionEvent event) {
+//                    return false;
+//                }
+//            });
+//            //popup animation
+////            popupWindow.setAnimationStyle(R.style.popup_window_anim);
+//            //transition is much more customizable than setAnimationStyle() but there is api level constraint
+////            popupWindow.setEnterTransition(getTransition(Gravity.BOTTOM));
+////            popupWindow.setExitTransition(getTransition(Gravity.RIGHT));
+//            //listen popup window dismiss event
+//            PopupWindow.OnDismissListener onDismissListener = new PopupWindow.OnDismissListener() {
+//                @Override
+//                public void onDismiss() {
+//                    Log.v("ttaylor", "WindowActivity.onDismiss()" + "  ");
+//                    changeBackgroundAlpha(WindowActivity.this, 1.0f);
+//                }
+//            };
+//            popupWindow.setOnDismissListener(onDismissListener);
+//        }
+//
+//        if (popupWindow.isShowing()) {
+//            popupWindow.dismiss();
+//            changeBackgroundAlpha(this, 1.0f);
+//        } else {
+//            popupWindow.showAtLocation(this.getWindow().getDecorView(), Gravity.BOTTOM, 0, 0);
+//            changeBackgroundAlpha(this, 0.5f);
+//        }
+
+    }
+
+
+    public void showBottomPopupWindow2(Context context) {
+        if (popupWindow == null) {
+            View windowView = getWindowView(context);
+            popupWindow = new CustomPopupWindow((ViewGroup) windowView,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, this);
+
+            popupWindow.setOutsideTouchable(true);
+            popupWindow.setOnItemClickListener(this);
+        }
+
+        if (popupWindow.isShowing()) {
+            popupWindow.dismiss();
+        } else {
+            popupWindow.showAtLocation(this.getWindow().getDecorView(), Gravity.BOTTOM, 0, 0);
+        }
+
+    }
+
+    private void bindWindowClickListener(View rootView) {
+        rootView.findViewById(R.id.btn_sexual_content).setOnClickListener(this);
+        rootView.findViewById(R.id.btn_violent).setOnClickListener(this);
+        rootView.findViewById(R.id.btn_spam).setOnClickListener(this);
+        rootView.findViewById(R.id.btn_cancel).setOnClickListener(this);
+
+    }
+
+    private void changeBackgroundAlpha(Activity activity, float alpha) {
+        Window window = activity.getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.alpha = alpha;
+        window.setAttributes(layoutParams);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_show_window:
+                showWindow(this);
+                break;
+            case R.id.btn_show_popup_window:
+                showBottomPopupWindow2(this);
+                break;
+            case R.id.btn_violent:
+                Log.v("ttaylor", "WindowActivity.onClick()" + "  violent");
+                popupWindow.dismiss();
+                break;
+            case R.id.btn_sexual_content:
+                Log.v("ttaylor", "WindowActivity.onClick()" + "  sexual");
+                popupWindow.dismiss();
+                break;
+            case R.id.btn_spam:
+                Log.v("ttaylor", "WindowActivity.onClick()" + "  spam");
+                popupWindow.dismiss();
+                break;
+            case R.id.btn_cancel:
+                Log.v("ttaylor", "WindowActivity.onClick()" + "  ");
+                popupWindow.dismiss();
+                break;
+        }
+    }
+
+    @Override
+    public void onWindowItemClick(View view) {
+        Log.v("ttaylor", "WindowActivity.onWindowItemClick()" + "  ");
+        int id = view.getId();
+        switch (id) {
+            case R.id.btn_violent:
+                Toast.makeText(this,"btn_violent",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_sexual_content:
+                Toast.makeText(this,"btn_sexual_content",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_spam:
+                Toast.makeText(this,"btn_spam",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_cancel:
+                Toast.makeText(this,"btn_cancel",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
