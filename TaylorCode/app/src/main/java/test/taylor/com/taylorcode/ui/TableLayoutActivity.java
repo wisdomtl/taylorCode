e87ab4e2c32f9b422a99e4bb341816bfc63c280b
taylor <liang.tang@cootek.cn>
2018-10-19 17:01:34 +0800

    [feat] show divider in TableLayout

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/TableLayoutActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/TableLayoutActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import test.taylor.com.taylorcode.R;
+
 public class TableLayoutActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_layout_activity);
     }
 }
