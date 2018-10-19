413507add804e36c9e21f26aa790f932718d8015
taylor <liang.tang@cootek.cn>
2018-10-19 16:27:34 +0800

    [feat] table

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/TableLayoutActivity.java
@@ -0,0 +1,12 @@
+package test.taylor.com.taylorcode.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class TableLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
