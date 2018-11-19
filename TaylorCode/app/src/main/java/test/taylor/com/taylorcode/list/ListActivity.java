1c8fe82eb75bef67676c9785e9bc69e05051113b
taylor <liang.tang@cootek.cn>
2018-11-19 18:17:10 +0800

    [feat] remove list header and list tail

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/list/ListActivity.java
@@ -0,0 +1,50 @@
+package test.taylor.com.taylorcode.list;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.util.LinkedList;
+
+public class ListActivity extends Activity {
+
+    private LinkedList<Integer> linkedLists = new LinkedList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //LinkedList case1:remove list header and list tail
+        initLinkedList();
+        removeLinkedListHeader();
+        removeLinkedListTail();
+
+    }
+
+    private void removeLinkedListTail() {
+        linkedLists.removeFirst();
+        for (int i :
+                linkedLists) {
+
+            Log.v("ttaylor", "ListActivity.removeLinkedListTail() after remove first" + "  i = " + i);
+        }
+        linkedLists.removeLast();
+        for (int i :
+                linkedLists) {
+            Log.v("ttaylor", "ListActivity.removeLinkedListTail() after remove last" + "  i = " + i);
+        }
+    }
+
+    private void removeLinkedListHeader() {
+    }
+
+    private void initLinkedList() {
+        linkedLists.add(1);
+        linkedLists.add(2);
+        linkedLists.add(3);
+        linkedLists.add(4);
+        linkedLists.add(5);
+        linkedLists.add(6);
+    }
+}
