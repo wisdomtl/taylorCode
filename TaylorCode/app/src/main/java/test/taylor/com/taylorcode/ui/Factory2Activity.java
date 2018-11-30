1ee7e5503baa71cb2346c94dcf7446bc8bdff5a9
taylor <liang.tang@cootek.cn>
2018-11-30 12:17:30 +0800

    [feat] hook view creation by Factory2

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/Factory2Activity.java
@@ -0,0 +1,53 @@
+package test.taylor.com.taylorcode.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import test.taylor.com.taylorcode.R;
+
+public class Factory2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        //hook view case1:hook view from LayoutInflater which will affect all the view in this activity
+        LayoutInflaterCompat.setFactory2(LayoutInflater.from(this), new LayoutInflater.Factory2() {
+            @Override
+            public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+                AppCompatDelegate delegate = getDelegate();
+                View view = delegate.createView(parent, name, context, attrs);
+                if (view instanceof TextView) {
+                    ((TextView) view).setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+            @Override
+            public View onCreateView(String name, Context context, AttributeSet attrs) {
+                return null;
+            }
+        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.factory2_activity);
+
+        findViewById(R.id.btn_start_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Factory2Activity.this, DialogActivity.class) ;
+                startActivity(intent);
+            }
+        });
+        View view = LayoutInflater.from(this).inflate(R.layout.factory_view, null);
+        ((ViewGroup) findViewById(R.id.container)).addView(view);
+    }
+}
