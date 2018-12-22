8e32494d413611894ac19ec6650df9664e223969
taylor <liang.tang@cootek.cn>
2018-12-22 15:56:24 +0800

    [feat] take picture ,store it in the sdcard and show it in gallery

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/TakePhotoActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/TakePhotoActivity.java
@@ -1,20 +1,38 @@
 package test.taylor.com.taylorcode.photo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.media.Image;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import test.taylor.com.taylorcode.R;
 
 public class TakePhotoActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int REQUEST_CODE_TAKE_PICTURE = 1;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private Uri imageUri;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +44,7 @@ public class TakePhotoActivity extends AppCompatActivity implements View.OnClick
     private void initView() {
         findViewById(R.id.btn_take_photo).setOnClickListener(this);
         findViewById(R.id.btn_pick_photo).setOnClickListener(this);
+        findViewById(R.id.btn_take_photo_store).setOnClickListener(this);
     }
 
     @Override
@@ -36,6 +55,21 @@ public class TakePhotoActivity extends AppCompatActivity implements View.OnClick
             case R.id.btn_take_photo:
                 takePicture();
                 break;
+            case R.id.btn_take_photo_store:
+                verifyStoragePermission(this);
+                break;
+        }
+    }
+
+    private void takePictureAndStore() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File imageFile = createImageFile();
+            imageUri = getImageUri(imageFile);
+            if (imageUri != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, REQUEST_CODE_TAKE_PICTURE);
+            }
         }
     }
 
@@ -50,9 +84,77 @@ public class TakePhotoActivity extends AppCompatActivity implements View.OnClick
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_TAKE_PICTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap bitmap = ((Bitmap) extras.get("data"));
-            ((ImageView) findViewById(R.id.iv_pic)).setImageBitmap(bitmap);
+            //show thumbnail
+            if (data != null) {
+                Bundle extras = data.getExtras();
+                Bitmap bitmap = ((Bitmap) extras.get("data"));
+                ((ImageView) findViewById(R.id.iv_pic)).setImageBitmap(bitmap);
+            }
+            //show origin image
+            if (imageUri != null) {
+                try {
+                    Bitmap bitmap1 = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                    ((ImageView) findViewById(R.id.iv_pic_origin)).setImageBitmap(bitmap1);
+                    showImageInGallery(imageUri);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    private void showImageInGallery(Uri imageUri) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(imageUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private File createImageFile() {
+        String imageFileName = String.valueOf(System.currentTimeMillis());
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File imageFile = null;
+        try {
+            imageFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return imageFile;
+    }
+
+    private Uri getImageUri(File imageFile) {
+        if (imageFile == null) {
+            return null;
+        }
+        Uri imageUri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            imageUri = FileProvider.getUriForFile(this, "com.taylor.fileProvider", imageFile);
+        } else {
+            imageUri = Uri.fromFile(imageFile);
+        }
+        return imageUri;
+    }
+
+    public void verifyStoragePermission(AppCompatActivity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_TAKE_PICTURE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePictureAndStore();
+                } else {
+                }
+            }
         }
     }
 }
