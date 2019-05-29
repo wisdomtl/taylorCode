0fc0911f5a1712f313c36ed63341cfb609cda0fa
taylor <liang.tang@cootek.cn>
2019-05-29 13:00:32 +0800

    [teat] startActivityForResult()+setResult()

---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/SetResultActivity.java
@@ -0,0 +1,45 @@
+package test.taylor.com.taylorcode;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class SetResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activit1);
+
+        findViewById(R.id.btnFinish).setOnClickListener(v -> finish());
+    }
+
+    @Override
+    protected void onStop() {
+        Log.v("ttaylor", "SetResultActivity.onStop()" + "  ");
+        setResult(111);
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.v("ttaylor", "SetResultActivity.onPause()" + "  ");
+        setResult(222);
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.v("ttaylor", "SetResultActivity.onBackPressed()" + "  ");
+        setResult(333);
+        super.onBackPressed();
+    }
+
+    @Override
+    public void finish() {
+        Log.v("ttaylor", "SetResultActivity.finish()" + "  ");
+        setResult(444);
+        super.finish();
+    }
+}
--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/DrawerLayoutActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/DrawerLayoutActivity.java
@@ -1,37 +1,56 @@
 package test.taylor.com.taylorcode.ui;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import test.taylor.com.taylorcode.R;
+import test.taylor.com.taylorcode.SetResultActivity;
 
 /**
  * Created on 2017/11/19.
  */
 
 public class DrawerLayoutActivity extends Activity {
-    public static final String [] DRAWER_CONTENT = new String[]{"caidan 1" ,"caidan 2","caidan 3","caidan 4"} ;
-    private DrawerLayout dl ;
+    public static final String[] DRAWER_CONTENT = new String[]{"caidan 1", "caidan 2", "caidan 3", "caidan 4"};
+    private DrawerLayout dl;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_layout_activity);
         ListView lv = ((ListView) findViewById(R.id.lv_drawer_content));
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.tv,DRAWER_CONTENT) ;
-         dl = ((DrawerLayout) findViewById(R.id.dl));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.tv, DRAWER_CONTENT);
+        dl = ((DrawerLayout) findViewById(R.id.dl));
         lv.setAdapter(adapter);
-        View drawerActivator = findViewById(R.id.tv_drawer_activator) ;
+        View drawerActivator = findViewById(R.id.tv_drawer_activator);
         drawerActivator.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   dl.openDrawer(Gravity.LEFT);
+                dl.openDrawer(Gravity.LEFT);
             }
         });
+
+
+        findViewById(R.id.btnSetResult).setOnClickListener(v -> {
+            startActivity(new Intent(DrawerLayoutActivity.this, SetResultActivity.class));
+        });
+        findViewById(R.id.btnForResult).setOnClickListener(v -> {
+            startActivityForResult(new Intent(DrawerLayoutActivity.this, SetResultActivity.class),200);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.v("ttaylor", "DrawerLayoutActivity.onActivityResult()" + "  requestCode="+requestCode+" resultCode="+resultCode);
     }
 }
