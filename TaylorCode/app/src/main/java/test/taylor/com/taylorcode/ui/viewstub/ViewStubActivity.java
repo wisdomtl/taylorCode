7d7fcc65cff9fcdabb60eab0f02321b080e647bf
taylor <liang.tang@cootek.cn>
2019-06-04 20:15:40 +0800

    [feat] slide in bottom and slide out in bottom at same time

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/viewstub/ViewStubActivity.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/viewstub/ViewStubActivity.java
@@ -20,6 +20,7 @@ import test.taylor.com.taylorcode.R;
 import test.taylor.com.taylorcode.util.DimensionUtil;
 
 public class ViewStubActivity extends AppCompatActivity {
+    private int duration = 400;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,19 +39,15 @@ public class ViewStubActivity extends AppCompatActivity {
         View vsRoot = vs.inflate();
         vsRoot.setVisibility(View.VISIBLE);
         doBottomInAnim(vsRoot);
+        hideBottomView(findViewById(R.id.tvooo));
     }
 
     private void doBottomInAnim(View view) {
-        final int duration = 100;
         view.postDelayed(() -> {
             int height = view.getHeight();
             int bottom = view.getBottom();
             Log.v("ttaylor", "ViewStubActivity.doBottomInAnim()" + "  bottom=" + bottom + " height=" + height);
             AnimatorSet animatorSet = new AnimatorSet();
-//            ObjectAnimator alpha = ObjectAnimator.ofFloat(view, "alpha", 0, 255);
-//            alpha.setDuration(duration);
-//            ObjectAnimator translationY = ObjectAnimator.ofFloat(view, "translationY", top, top - height);
-//            translationY.setDuration(duration);
             ValueAnimator animator = ValueAnimator.ofFloat(0, 1.0f);
             animator.setInterpolator(new AccelerateDecelerateInterpolator());
             animator.setDuration(duration);
@@ -70,6 +67,33 @@ public class ViewStubActivity extends AppCompatActivity {
             animatorSet.playTogether(animator, animator2);
             animatorSet.start();
         },0);
+    }
+
+    private void hideBottomView(View view){
+        view.postDelayed(() -> {
+            int height = view.getHeight();
+            Log.v("ttaylor", "ViewStubActivity.hideBottomView()" + "  height="+height);
+            AnimatorSet animatorSet = new AnimatorSet();
+            ValueAnimator animator = ValueAnimator.ofFloat(1.0f, 0);
+            animator.setInterpolator(new AccelerateDecelerateInterpolator());
+            animator.setDuration(duration);
+            animator.addUpdateListener(animation -> {
+                float size = (Float) animation.getAnimatedValue();
+                view.setAlpha(size);
+            });
+
+
+            ValueAnimator animator2 = ValueAnimator.ofFloat(0, height);
+            animator2.setInterpolator(new AccelerateDecelerateInterpolator());
+            animator2.setDuration(duration);
+            animator2.addUpdateListener(animation -> {
+                float size = (Float) animation.getAnimatedValue();
+                view.setTranslationY(size);
+            });
+            animatorSet.playTogether(animator, animator2);
+            animatorSet.start();
+        },0);
+
     }
 
 }
