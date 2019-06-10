add1d9943d68781694787793252f3fbe9227bc05
taylor <liang.tang@cootek.cn>
2019-06-10 17:53:06 +0800

    [feat] do animation with Animator more efficient

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/transparent_fragment/TransparentFragment.java
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/transparent_fragment/TransparentFragment.java
@@ -1,6 +1,8 @@
 package test.taylor.com.taylorcode.ui.transparent_fragment;
 
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.renderscript.Sampler;
@@ -23,47 +25,57 @@ public class TransparentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.transparent_fragment,container,false);
+        return inflater.inflate(R.layout.transparent_fragment, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        showOpeningAnim(view.findViewById(R.id.tvTop),view.findViewById(R.id.tvBottom));
+        showOpeningAnim(view.findViewById(R.id.tvTop), view.findViewById(R.id.tvBottom));
 
     }
 
-    private void showOpeningAnim(View topView ,View bottomView) {
+    private void showOpeningAnim(View topView, View bottomView) {
         bottomView.setAlpha(0f);
         topView.setAlpha(0f);
         bottomView.post(() -> {
             int height = bottomView.getHeight();
-            ValueAnimator animator = ValueAnimator.ofFloat(0, 1.0f);
-            animator.setInterpolator(new AccelerateInterpolator());
-            animator.setDuration(OPENING_ANIM_DURATION);
-            animator.addUpdateListener(animation -> {
-                float size = (Float) animation.getAnimatedValue();
-                bottomView.setAlpha(size);
-            });
+            PropertyValuesHolder translationHolder = PropertyValuesHolder.ofFloat("translationY", 0, -height);
+            PropertyValuesHolder alphaValueHolder = PropertyValuesHolder.ofFloat("alpha", 0, 1.0f);
+            ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(bottomView, translationHolder, alphaValueHolder);
+            objectAnimator.setDuration(OPENING_ANIM_DURATION);
+            objectAnimator.setInterpolator(new AccelerateInterpolator());
 
-            ValueAnimator animator2 = ValueAnimator.ofFloat(0, -height);
-            animator2.setInterpolator(new AccelerateDecelerateInterpolator());
-            animator2.setDuration(OPENING_ANIM_DURATION);
-            animator2.addUpdateListener(animation -> {
-                float size = (Float) animation.getAnimatedValue();
-                bottomView.setTranslationY(size);
-            });
+//            ValueAnimator animator = ValueAnimator.ofFloat(0, 1.0f);
+//            animator.setInterpolator(new AccelerateInterpolator());
+//            animator.setDuration(OPENING_ANIM_DURATION);
+//            animator.addUpdateListener(animation -> {
+//                float size = (Float) animation.getAnimatedValue();
+//                bottomView.setAlpha(size);
+//            });
+//
+//            ValueAnimator animator2 = ValueAnimator.ofFloat(0, -height);
+//            animator2.setInterpolator(new AccelerateDecelerateInterpolator());
+//            animator2.setDuration(OPENING_ANIM_DURATION);
+//            animator2.addUpdateListener(animation -> {
+//                float size = (Float) animation.getAnimatedValue();
+//                bottomView.setTranslationY(size);
+//            });
+            PropertyValuesHolder alphaValueHolder2 = PropertyValuesHolder.ofFloat("alpha", 0, 1.0f);
+            ObjectAnimator objectAnimator1 = ObjectAnimator.ofPropertyValuesHolder(topView, alphaValueHolder2);
+            objectAnimator1.setDuration(OPENING_ANIM_DURATION);
+            objectAnimator1.setInterpolator(new AccelerateInterpolator());
 
-            ValueAnimator topAnim = ValueAnimator.ofFloat(0,1.0f);
-            topAnim.setInterpolator(new AccelerateInterpolator());
-            topAnim.setDuration(OPENING_ANIM_DURATION);
-            topAnim.addUpdateListener(animation -> {
-                float size = (Float)animation.getAnimatedValue();
-                topView.setAlpha(size);
-            });
+//            ValueAnimator topAnim = ValueAnimator.ofFloat(0, 1.0f);
+//            topAnim.setInterpolator(new AccelerateInterpolator());
+//            topAnim.setDuration(OPENING_ANIM_DURATION);
+//            topAnim.addUpdateListener(animation -> {
+//                float size = (Float) animation.getAnimatedValue();
+//                topView.setAlpha(size);
+//            });
 
             AnimatorSet openingAnim = new AnimatorSet();
-            openingAnim.playTogether(topAnim,animator,animator2);
+            openingAnim.playTogether(objectAnimator1, objectAnimator);
             openingAnim.start();
         });
     }
