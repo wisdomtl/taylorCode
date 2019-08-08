6bff52fad8c70c1a8c038d9f5aac36aba7997a72
taylor <liang.tang@cootek.cn>
2019-08-08 12:21:44 +0800

    [feat] kotlin list equal

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/KotlinActivity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/KotlinActivity.kt
@@ -2,16 +2,25 @@ package test.taylor.com.taylorcode.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.constraint_layout_activity.*
 import test.taylor.com.taylorcode.R
 
 class KotlinActivity : AppCompatActivity() {
+    val list1 = listOf<String>("abd", "add", "fff")
+    val list2 = listOf<String>("abd", "add", "fff")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.constraint_layout_activity)
-        btn3.setOnClickListener {Toast.makeText(this,"onclick for kotlin",Toast.LENGTH_LONG).show()}
+        btn3.setOnClickListener { Toast.makeText(this, "onclick for kotlin", Toast.LENGTH_LONG).show() }
 
+        listEquals()
+
+    }
+
+    private fun listEquals() {
+        Log.v("ttaylor", "tag=equals, KotlinActivity.listEquals() isEquals=${list1 == list2}")
     }
 }
