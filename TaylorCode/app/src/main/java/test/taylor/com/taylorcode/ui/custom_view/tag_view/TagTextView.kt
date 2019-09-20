96dbad41bcbb8e834f05bd7dc0a58e0bf23463c1
taylor <liang.tang@cootek.cn>
2019-09-20 14:13:55 +0800

    [feat] red point TagTextView

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/tag_view/TagTextView.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/tag_view/TagTextView.kt
@@ -1,14 +1,11 @@
 package test.taylor.com.taylorcode.ui.custom_view.tag_view
 
 import android.content.Context
-import android.content.res.Resources
 import android.graphics.*
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
-import android.util.DisplayMetrics
-import android.util.TypedValue
-import android.widget.TextView
 
-class TagTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : TextView(context, attrs, defStyleAttr) {
+open class TagTextView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatTextView(context, attrs, defStyleAttr) {
 
     var tagBgColor: Int = Color.parseColor("#FFFF5183")
         set(value) {
@@ -20,49 +17,65 @@ class TagTextView @JvmOverloads constructor(context: Context, attrs: AttributeSe
             field = value
             textPaint.textSize = value
         }
-    var tagTextColor: Int = Color.parseColor("#ffffff")
+    var tagTextColor: Int = Color.parseColor("#FFFFFF")
         set(value) {
             field = value
             textPaint.color = value
         }
+    var tagTextTypeFace: Typeface? = null
 
     var tagText: String? = null
-    var tagTextPaddingTop: Float = 0f
-    var tagTextPaddingBottom: Float = 0f
-    var tagTextPaddingStart: Float = 0f
-    var tagTextPaddingEnd: Float = 0f
+    var tagTextPaddingTop: Float = 5f
+    var tagTextPaddingBottom: Float = 5f
+    var tagTextPaddingStart: Float = 5f
+    var tagTextPaddingEnd: Float = 5f
 
     private var textRect: Rect = Rect()
     private var bgPaint: Paint = Paint()
     private var textPaint: Paint = Paint()
 
     init {
-        bgPaint.isAntiAlias = true
-        bgPaint.style = Paint.Style.FILL
-        bgPaint.color = tagBgColor
-        textPaint.color = tagTextColor
-        textPaint.textSize = tagTextSize
-        textPaint.isAntiAlias = true
-        textPaint.textAlign = Paint.Align.CENTER
-        textPaint.style = Paint.Style.FILL
+        bgPaint.apply {
+            isAntiAlias = true
+            style = Paint.Style.FILL
+            color = tagBgColor
+        }
+        textPaint.apply {
+            color = tagTextColor
+            textSize = tagTextSize
+            isAntiAlias = true
+            textAlign = Paint.Align.CENTER
+            style = Paint.Style.FILL
+            tagTextTypeFace?.let { typeface = tagTextTypeFace }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        if (oldw != 0 && oldh != 0) {
+            val widthRatio = w.toFloat() / oldw
+            widthRatio.takeIf { it > 0 }?.let { tagTextSize *= widthRatio }
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         tagText?.takeIf { it.isNotEmpty() }?.let { text ->
-            textPaint.getTextBounds(text, 0, text.length, textRect)
-            textPaint.fontMetricsInt.let {
-                val bgWidth = (textRect.right - textRect.left) + tagTextPaddingStart + tagTextPaddingEnd
-                val bgHeight = tagTextPaddingBottom + tagTextPaddingTop + it.bottom - it.top
+            textPaint.apply {
+                getTextBounds(text, 0, text.length, textRect)
+                fontMetricsInt.let {
+                    val bgWidth = (textRect.right - textRect.left) + tagTextPaddingStart + tagTextPaddingEnd
+                    val bgHeight = tagTextPaddingBottom + tagTextPaddingTop + it.bottom - it.top
 
-                val radius = if (bgWidth > bgHeight) bgWidth / 2 else bgHeight / 2
-                val centerX = width - radius
-                val centerY = radius
-                canvas?.drawCircle(centerX, centerY, radius, bgPaint)
+                    val radius = if (bgWidth > bgHeight) bgWidth / 2 else bgHeight / 2
+                    val centerX = width - radius
+                    val centerY = radius
+                    canvas?.drawCircle(centerX, centerY, radius, bgPaint)
 
-                val textHeight = it.bottom - it.top
-                val baseline = radius  + (textHeight / 2 - it.bottom)
-                canvas?.drawText(text, width - radius, baseline, textPaint)
+                    val textHeight = it.bottom - it.top
+                    val baseline = radius + (textHeight / 2 - it.bottom)
+                    canvas?.drawText(text, width - radius, baseline, textPaint)
+                }
             }
         }
     }
--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/tag_view/TagTextViewActivity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/tag_view/TagTextViewActivity.kt
@@ -3,6 +3,7 @@ package test.taylor.com.taylorcode.ui.custom_view.tag_view
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.DisplayMetrics
@@ -11,19 +12,31 @@ import kotlinx.android.synthetic.main.tag_textview_activity.*
 import test.taylor.com.taylorcode.R
 
 class TagTextViewActivity:AppCompatActivity() {
+    private val pingfang by lazy {
+        Typeface.createFromAsset(assets, "pingfang.ttf")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tag_textview_activity)
 
 
-        ttv_1.tagText = "  "
+        ttv_1.tagText = "123"
         ttv_1.tagTextSize = dip(8F)
         ttv_1.tagTextColor = Color.YELLOW
+        ttv_1.tagTextTypeFace = pingfang
 //        ttv_1.tagTextPaddingStart = dip(3f)
 //        ttv_1.tagTextPaddingEnd = dip(3f)
 //        ttv_1.tagTextPaddingTop = dip(1f)
 //        ttv_1.tagTextPaddingBottom = dip(1f)
+
+        btnLarger.setOnClickListener {
+            ttv_1.setTextSize(dip(35f))
+        }
+
+        btnSmaller.setOnClickListener {
+            ttv_1.setTextSize(dip(10f))
+        }
     }
 }
 
