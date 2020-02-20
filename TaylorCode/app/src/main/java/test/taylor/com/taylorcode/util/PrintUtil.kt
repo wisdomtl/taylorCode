b8e2c7dd4468caa6a52bfb4e058af6bbec98a5a2
taylor <liang.tang@cootek.cn>
2020-02-20 10:57:09 +0800

    Feat: a beautiful logger

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/util/PrintUtil.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/util/PrintUtil.kt
@@ -3,11 +3,17 @@ package test.taylor.com.taylorcode.util
 import android.util.Log
 import test.taylor.com.taylorcode.BuildConfig
 
+/**
+ * print method call stack by [deep]
+ */
 fun printCallStack(tag: String, deep: Int) {
     if (!BuildConfig.DEBUG) return
     Throwable().stackTrace?.take(deep)?.print(tag) { it -> "${it.className}.${it.methodName} (line ${it.lineNumber})" }
 }
 
+/**
+ * print collection
+ */
 fun <T> Collection<T>.print(tag: String, map: (T) -> String) {
     if (!BuildConfig.DEBUG) return
     this.let { c ->
