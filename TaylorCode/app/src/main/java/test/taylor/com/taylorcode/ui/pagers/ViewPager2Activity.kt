73dddef6b3dacece9a6b44c5f2754f81e08935b7
taylor <liang.tang@cootek.cn>
2020-03-26 10:28:10 +0800

    [feat] basic case of ViewPager2 with Fragment

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/pagers/ViewPager2Activity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/pagers/ViewPager2Activity.kt
@@ -2,14 +2,19 @@ package test.taylor.com.taylorcode.ui.pagers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.viewpager2_activity.*
 import test.taylor.com.taylorcode.R
 
-class ViewPager2Activity:AppCompatActivity() {
+class ViewPager2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.viewpager2_activity)
-        vp2.adapter = ViewPager2Adapter(supportFragmentManager,lifecycle)
+        vp2.adapter = ViewPager2Adapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(tablayout, vp2) { tab, position ->
+            tab.text = position.toString()
+            tab.badge
+        }.attach()
     }
 }
