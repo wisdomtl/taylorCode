2f51dfb4cd3590e759bde616b5b19b253f45bdcc
wisdomtl <l.tang@neoclub.cn>
2021-02-26 15:08:00 +0800

    Feat: crop bitmap with radii

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/clip_path/Crop.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/clip_path/Crop.kt
@@ -6,7 +6,8 @@ import test.taylor.com.taylorcode.kotlin.dp
 
 
 /**
- * crop [Bitmap] to round [Bitmap]. if [radius] is big enough, it will become a circle
+ * crop [Bitmap] to round [Bitmap]. if [radius] is big enough, it becomes a circle.
+ * implement by [Shader]
  */
 fun Bitmap.crop(radius: Float): Bitmap =
     Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888).applyCanvas {
@@ -20,3 +21,35 @@ fun Bitmap.crop(radius: Float): Bitmap =
         )
         setBitmap(null)
     }
+
+/**
+ * crop [Bitmap] to round [Bitmap], you could specify which corner to be round
+ * implement by [Path]
+ */
+fun Bitmap.crop2(corners: Corners): Bitmap =
+    Bitmap.createBitmap(this.width, this.height, Bitmap.Config.ARGB_8888).applyCanvas {
+        val path = Path().apply {
+            addRoundRect(
+                RectF(0f, 0f, this@crop2.width.toFloat(), this@crop2.height.toFloat()),
+                corners.radii,
+                Path.Direction.CCW
+            )
+        }
+        clipPath(path)
+        drawBitmap(this@crop2, 0f, 0f, Paint(Paint.ANTI_ALIAS_FLAG))
+        setBitmap(null)
+    }
+
+class Corners(
+    var leftTopRx: Float = 0f,
+    var leftTopRy: Float = 0f,
+    var leftBottomRx: Float = 0f,
+    var LeftBottomRy: Float = 0f,
+    var rightTopRx: Float = 0f,
+    var rightTopRy: Float = 0f,
+    var rightBottomRx: Float = 0f,
+    var rightBottomRy: Float = 0f
+) {
+    val radii: FloatArray
+        get() = floatArrayOf(leftTopRx, leftTopRy, rightTopRx, rightTopRy, rightBottomRx, rightBottomRy, leftBottomRx, LeftBottomRy)
+}
--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/shader/ShaderActivity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/custom_view/shader/ShaderActivity.kt
@@ -7,7 +7,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import test.taylor.com.taylorcode.R
 import test.taylor.com.taylorcode.kotlin.*
+import test.taylor.com.taylorcode.ui.custom_view.clip_path.Corners
 import test.taylor.com.taylorcode.ui.custom_view.clip_path.crop
+import test.taylor.com.taylorcode.ui.custom_view.clip_path.crop2
 
 class ShaderActivity : AppCompatActivity() {
 
@@ -18,12 +20,12 @@ class ShaderActivity : AppCompatActivity() {
 
             ImageView {
                 layout_id = "tvChange"
-                layout_width = 100
-                layout_height = 100
+                layout_width = 300
+                layout_height = 300
                 scaleType = scale_fit_xy
                 center_horizontal = true
                 center_vertical = true
-                bitmap = getMutableBitmap(R.drawable.p1).crop(300f)
+                bitmap = getMutableBitmap(R.drawable.p1).crop2(Corners(leftTopRx = 400f,leftTopRy = 100f))
             }
         }
     }
