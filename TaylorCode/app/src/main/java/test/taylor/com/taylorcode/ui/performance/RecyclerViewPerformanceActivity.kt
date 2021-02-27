8d4be501de13aef6d1d9e28b87d9484d8a9e8d54
wisdomtl <l.tang@neoclub.cn>
2021-02-27 13:09:11 +0800

    Feat: make multiple item in one item in RecyclerView for performance sake

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/performance/RecyclerViewPerformanceActivity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/performance/RecyclerViewPerformanceActivity.kt
@@ -11,12 +11,15 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import test.taylor.com.taylorcode.kotlin.*
+import test.taylor.com.taylorcode.ui.performance.better_performance.BetterRank
+import test.taylor.com.taylorcode.ui.performance.better_performance.BetterRankProxy
 import test.taylor.com.taylorcode.ui.recyclerview.variety.VarietyAdapter2
 
 class RecyclerViewPerformanceActivity : AppCompatActivity() {
     private val myAdapter = VarietyAdapter2().apply {
         addProxy(HeaderProxy())
         addProxy(RankProxy())
+        addProxy(BetterRankProxy())
     }
 
     private val contentView by lazy {
@@ -44,10 +47,147 @@ class RecyclerViewPerformanceActivity : AppCompatActivity() {
 
         detectFrame()
 
-        bindData()
+//        bindData1()
+        bindData2()
     }
 
-    private fun bindData() {
+    private fun bindData2() {
+        myAdapter.dataList = listOf(
+            BetterRank(
+                "排名", "主播", "粉丝数", "本周新增",
+                listOf(
+                    Rank(
+                        1,
+                        "小红",
+                        19203,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F02%2F08%2F14%2F5a44ec4e1c829.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931237&t=a265e1a71e69f2b2d57e7dcc5cb5b769",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        2,
+                        "小红",
+                        309293,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F00%2F58%2F41%2F78%2F5933cf3f09c71.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616930901&t=3b79951168c9d6bfc69233c2c8eb0c7e",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        3,
+                        "小里",
+                        193930,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F00%2F58%2F41%2F78%2F5933cf3f09c71.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616930901&t=3b79951168c9d6bfc69233c2c8eb0c7e",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        4,
+                        "小腾",
+                        1999990,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F02%2F08%2F14%2F5a44ec4e1c829.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931237&t=a265e1a71e69f2b2d57e7dcc5cb5b769",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        5,
+                        "天青色",
+                        19939330,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F00%2F58%2F41%2F78%2F5933cf3f09c71.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616930901&t=3b79951168c9d6bfc69233c2c8eb0c7e",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        6,
+                        "等烟雨来,就是不来",
+                        19090990,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpngimg.com%2Fuploads%2Fxbox%2Fxbox_PNG17507.png&refer=http%3A%2F%2Fpngimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931278&t=3faf6ba95b0ea1fedd21bb7ccc965477",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        7,
+                        "急死你,我就不来",
+                        190,
+                        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1405706906,3786206755&fm=26&gp=0.jpg",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        8,
+                        "你拿我怎么办呢,嘿嘿",
+                        190,
+                        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3544022885,3616110694&fm=26&gp=0.jpg",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        9,
+                        "不来就不来,爱来不来",
+                        138239,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F00%2F58%2F41%2F78%2F5933cf3f09c71.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616930901&t=3b79951168c9d6bfc69233c2c8eb0c7e",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        10,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        11,
+                        "不去吧,,d,d,d,nazme",
+                        3223,
+                        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1587290859,1459340053&fm=26&gp=0.jpg",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        12,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        13,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        14,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        15,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        16,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        17,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    ),
+                    Rank(
+                        18,
+                        "呵呵呵呵呵呵呵呵",
+                        382989238,
+                        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.90sjimg.com%2Fdesign%2F02%2F03%2F48%2F08%2F5a44fc71b9e36.png&refer=http%3A%2F%2Fpic.90sjimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1616931214&t=9efa218eec84e6ac2e37a65587f97738",
+                        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2729107397,409883431&fm=26&gp=0.jpg"
+                    )
+                )
+            )
+        )
+    }
+
+    private fun bindData1() {
         myAdapter.dataList = listOf(
             Header("排名", "主播", "粉丝数", "本周新增"),
             Rank(
@@ -188,6 +328,6 @@ fun Activity.detectFrame(){
                 "    anim=${frameMetrics.getMetric(FrameMetrics.ANIMATION_DURATION)/1000000}," +
                 "    touch=${frameMetrics.getMetric(FrameMetrics.INPUT_HANDLING_DURATION)/1000000}, " +
                 "    draw=${frameMetrics.getMetric(FrameMetrics.DRAW_DURATION)/1000000}, " +
-                "    total=${frameMetrics.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION)/1000000}")
+                "    total=${frameMetrics.getMetric(FrameMetrics.TOTAL_DURATION)/1000000}")
     }, Handler())
 }
---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/performance/better_performance/BetterRankProxy.kt
@@ -0,0 +1,176 @@
+package test.taylor.com.taylorcode.ui.performance.better_performance
+
+import android.graphics.drawable.Drawable
+import android.text.TextUtils
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import test.taylor.com.taylorcode.kotlin.*
+import test.taylor.com.taylorcode.ui.performance.Rank
+import test.taylor.com.taylorcode.ui.recyclerview.variety.VarietyAdapter2
+import kotlin.math.min
+
+class BetterRankProxy : VarietyAdapter2.Proxy<BetterRank, BetterRankViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView = parent.context.run {
+            LinearLayout {
+                layout_width = match_parent
+                layout_height = wrap_content
+                orientation = vertical
+                margin_start = 20
+                margin_end = 20
+                padding_top = 16
+                padding_bottom = 16
+                padding_start = 20
+                padding_end = 20
+                shape = shape {
+                    corner_radius = 20
+                    solid_color = "#ffffff"
+                }
+
+                TextView {
+                    layout_id = "tvFansRankTitle"
+                    layout_width = wrap_content
+                    layout_height = wrap_content
+                    textSize = 16f
+                    textColor = "#3F4658"
+                    textStyle = bold
+                    margin_bottom = 3
+                }
+
+                ConstraintLayout {
+                    layout_width = match_parent
+                    layout_height = wrap_content
+                    margin_top = 16
+
+                    TextView {
+                        layout_id = "tvRankColumn"
+                        layout_width = wrap_content
+                        layout_height = wrap_content
+                        textSize = 11f
+                        textColor = "#9DA4AD"
+                        start_toStartOf = parent_id
+                        center_vertical = true
+                    }
+
+                    TextView {
+                        layout_id = "tvAnchormanColumn"
+                        layout_width = wrap_content
+                        layout_height = wrap_content
+                        textSize = 11f
+                        textColor = "#9DA4AD"
+                        align_vertical_to = "tvRankColumn"
+                        start_toEndOf = "tvRankColumn"
+                        margin_start = 19
+                    }
+
+                    TextView {
+                        layout_id = "tvSumColumn"
+                        layout_width = wrap_content
+                        layout_height = wrap_content
+                        textSize = 11f
+                        textColor = "#9DA4AD"
+                        align_vertical_to = "tvRankColumn"
+                        end_toEndOf = parent_id
+                    }
+                }
+            }
+
+        }
+        return BetterRankViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: BetterRankViewHolder, data: BetterRank, index: Int, action: ((Any?) -> Unit)?) {
+        holder.tvAnchormanColumn?.text = data.nameColumn
+        holder.tvRankColumn?.text = data.rankColumn
+        holder.tvSumColumn?.text = data.countColumn
+        holder.tvTitle?.text = data.title
+
+        (holder.itemView as? LinearLayout)?.apply {
+            data.ranks.forEachIndexed { index, member ->
+                ConstraintLayout {
+                    layout_width = match_parent
+                    layout_height = 30
+
+
+                    TextView {
+                        layout_id = "vAnchormanRank"
+                        layout_width = 18
+                        layout_height = wrap_content
+                        textSize = 14f
+                        textColor = "#9DA4AD"
+                        gravity = gravity_center
+                        center_vertical = true
+                        start_toStartOf = parent_id
+                        margin_start = 2
+                        text = member.rank.toString()
+                    }
+
+                    ImageView {
+                        layout_id = "ivAnchormanAvatar"
+                        layout_width = 20
+                        layout_height = 20
+                        scaleType = scale_fit_xy
+                        center_vertical = true
+                        start_toEndOf = "vAnchormanRank"
+                        margin_start = 12
+                        Glide.with(this.context).load(member.avatarUrl).into(this)
+                    }
+
+                    TextView {
+                        layout_id = "tvAnchormanName"
+                        layout_width = wrap_content
+                        layout_height = wrap_content
+                        textSize = 12f
+                        textColor = "#3F4658"
+                        center_vertical = true
+                        start_toEndOf = "ivAnchormanAvatar"
+                        margin_start = 10
+                        maxLines = 1
+                        ellipsize = TextUtils.TruncateAt.END
+                        text = member.name
+                    }
+
+                    ImageView {
+                        layout_id = "ivLevel"
+                        layout_width = 15
+                        layout_height = 15
+                        scaleType = scale_fit_xy
+                        center_vertical = true
+                        start_toEndOf = "tvAnchormanName"
+                        margin_start = 5
+                    }
+
+                    TextView {
+                        layout_id = "tvFansCount"
+                        layout_width = wrap_content
+                        layout_height = wrap_content
+                        textSize = 12f
+                        textColor = "#747E8B"
+                        center_vertical = true
+                        end_toEndOf = parent_id
+                        text = member.count.toString()
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class BetterRank(
+    val title: String,
+    val rankColumn: String,
+    val nameColumn: String,
+    val countColumn: String,
+    val ranks: List<Rank>
+)
+
+class BetterRankViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tvTitle = itemView.find<TextView>("tvFansRankTitle")
+    val tvRankColumn = itemView.find<TextView>("tvRankColumn")
+    val tvAnchormanColumn = itemView.find<TextView>("tvAnchormanColumn")
+    val tvSumColumn = itemView.find<TextView>("tvSumColumn")
+}
