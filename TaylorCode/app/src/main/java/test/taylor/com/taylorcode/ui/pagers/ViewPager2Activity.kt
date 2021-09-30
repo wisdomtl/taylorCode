5c681865b4014bb062be95e6c2a1e8764d7df88f
wisdomtl <l.tang@neoclub.cn>
2021-09-30 18:19:47 +0800

    Feat: ViewPager2 with views

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/pagers/ViewPager2Activity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/pagers/ViewPager2Activity.kt
@@ -5,17 +5,22 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.viewpager2_activity.*
 import test.taylor.com.taylorcode.R
+import test.taylor.com.taylorcode.kotlin.TextView
+import test.taylor.com.taylorcode.kotlin.*
+import test.taylor.com.taylorcode.ui.recyclerview.variety.VarietyAdapter2
 
 class ViewPager2Activity : AppCompatActivity() {
 
     private val imgUrls = mutableListOf(
-        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3030050658,3694586235&fm=26&gp=0.jpg"
-        ,
-        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2571315283,182922750&fm=26&gp=0.jpg"
-        ,
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3030050658,3694586235&fm=26&gp=0.jpg",
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2571315283,182922750&fm=26&gp=0.jpg",
         "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1591790054139&di=627d2e1d16d93f1f2fdcac074a623d39&imgtype=0&src=http%3A%2F%2Fpngimg.com%2Fuploads%2Fdonald_trump%2Fdonald_trump_PNG56.png"
     )
 
@@ -36,10 +41,17 @@ class ViewPager2Activity : AppCompatActivity() {
         /**
          * case: ViewPager2 with views
          */
-        vp2.adapter = ViewPageViewAdapter().apply { data = imgUrls }
-        bannerViewPager()
+        vp2.adapter  = VarietyAdapter2().apply {
+            addProxy(ViewPagerProxy())
+            dataList = listOf(
+                "djflksdjfljsfjladskfl",
+                "23223432423543454356346534",
+                "eioroeiwurieurioweuroiuweruwoiruworui"
+            )
+        }
+//        bannerViewPager()
 
-        fl.dispatchEvent = {ev->
+        fl.dispatchEvent = { ev ->
             if (ev?.action == MotionEvent.ACTION_MOVE) {
                 handler.removeCallbacksAndMessages(null)
             } else if (ev?.action == MotionEvent.ACTION_UP) {
@@ -49,19 +61,54 @@ class ViewPager2Activity : AppCompatActivity() {
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        Log.v("ttaylor","tag=asdf, ViewPager2Activity.onTouchEvent()  event=${event?.action}")
+        Log.v("ttaylor", "tag=asdf, ViewPager2Activity.onTouchEvent()  event=${event?.action}")
         return super.onTouchEvent(event)
     }
 
 
+    /**
+     * case3: make ViewPager auto-scroll like banner
+     */
     val showNext = object : Runnable {
         override fun run() {
             vp2.currentItem = (++vp2.currentItem)
             handler.postDelayed(this, 1000)
         }
     }
+
     private fun bannerViewPager() {
         if (imgUrls.size <= 1) return
         handler.postDelayed(showNext, 1000)
     }
+}
+
+class ViewPagerProxy : VarietyAdapter2.Proxy<String, ViewPagerViewHolder2>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val itemView = parent.context.run {
+            TextView {
+                layout_id = "tvChange"
+                layout_width = match_parent
+                layout_height = match_parent
+                textSize = 50f
+                textColor = "#ffffff"
+                fontFamily = R.font.pingfang
+                gravity = gravity_center
+            }
+        }
+        return ViewPagerViewHolder2(itemView)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewPagerViewHolder2,
+        data: String,
+        index: Int,
+        action: ((Any?) -> Unit)?
+    ) {
+        holder.tv?.text = data
+    }
+
+}
+
+class ViewPagerViewHolder2(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val tv = itemView.find<TextView>("tvChange")
 }
