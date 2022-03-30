30dd41d6d1cbfc9143483ebd167045bc00e528c2
wisdomtl <l.tang@neoclub.cn>
2022-03-30 19:47:29 +0800

    Feat: add comment for nested scroll

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/material_design/nested/RecyclerViewBehavior.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/material_design/nested/RecyclerViewBehavior.kt
@@ -11,13 +11,28 @@ import kotlin.math.min
 
 class RecyclerViewBehavior @JvmOverloads constructor(context:Context,  attrs: AttributeSet) : CoordinatorLayout.Behavior<RecyclerView>(context, attrs) {
 
+    /**
+     * determine whether RecyclerView's layout should be depended on another view , which is the direct child of CoordinateLayout
+     * if true, the RecyclerView will be layout after the [dependency] view
+     */
     override fun layoutDependsOn(parent: CoordinatorLayout, child: RecyclerView, dependency: View): Boolean {
         return dependency is TextView
     }
 
+    /**
+     * define how RecyclerView should be layout by dependency View
+     */
     override fun onDependentViewChanged(parent: CoordinatorLayout, child: RecyclerView, dependency: View): Boolean {
         val y = dependency.height + dependency.translationY
         child.y = max(0f,y)
         return true
+    }
+
+    override fun onLayoutChild(
+        parent: CoordinatorLayout,
+        child: RecyclerView,
+        layoutDirection: Int
+    ): Boolean {
+        return super.onLayoutChild(parent, child, layoutDirection)
     }
 }
--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/material_design/nested/ScrollBehavior.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/material_design/nested/ScrollBehavior.kt
@@ -10,27 +10,57 @@ import androidx.core.view.ViewCompat
 import kotlin.math.max
 import kotlin.math.min
 
-class ScrollBehavior @JvmOverloads constructor(context: Context, attrs: AttributeSet) : CoordinatorLayout.Behavior<TextView>(context, attrs) {
+/**
+ * the Behavior set to the TextView, which is the direct child of CoordinateLayout
+ *
+ * the siblings of TextView is RecyclerView,which trigger the the nested scroll
+ *
+ * the TextView has the priority to consume the scroll event on [onNestedPreScroll], on the condition that [onStartNestedScroll] return true, which means this Behavior wishes to deal the the nested scroll event
+ */
+class ScrollBehavior @JvmOverloads constructor(context: Context, attrs: AttributeSet) :
+    CoordinatorLayout.Behavior<TextView>(context, attrs) {
 
-    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: TextView, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: TextView,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
         val ret = axes and ViewCompat.SCROLL_AXIS_VERTICAL != 0
         Log.v("ttaylor", "onStartNestedScroll ret = $ret")
         return axes and ViewCompat.SCROLL_AXIS_VERTICAL != 0
     }
 
-    override fun onNestedPreScroll(coordinatorLayout: CoordinatorLayout, child: TextView, target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: TextView,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
 
-        val translate = (child.translationY != -child.height.toFloat() && dy > 0)
+        Log.v("ttaylor", "onNestedPreScroll() dy=${dy} translate = ${child.translationY}")
+        val topTextViewCouldMove = (child.translationY != -child.height.toFloat() && dy > 0)
                 || (dy < 0 && child.translationY != 0f)
-        if (translate) {
+        if (topTextViewCouldMove) {
             val translationY = (child.translationY - dy).toInt()
             child.translationY = max(-child.height, min(translationY, 0)).toFloat()
-            consumed[1] = dy
+            consumed[1] = dy //let TextView consume the dy
         }
     }
 
-    override fun onNestedPreFling(coordinatorLayout: CoordinatorLayout, child: TextView, target: View, velocityX: Float, velocityY: Float): Boolean {
+    override fun onNestedPreFling(
+        coordinatorLayout: CoordinatorLayout,
+        child: TextView,
+        target: View,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
         return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY)
 
     }
