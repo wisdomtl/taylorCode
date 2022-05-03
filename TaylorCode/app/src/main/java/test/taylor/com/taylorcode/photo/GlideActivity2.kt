4c6dd3b5e7e76236a4d9141965572ac3e1b55853
wisdomtl <l.tang@neoclub.cn>
2022-05-03 21:02:33 +0800

    Feat: different disk cache strategy

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/GlideActivity2.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/GlideActivity2.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions
-import com.bumptech.glide.request.RequestOptions
 import test.taylor.com.taylorcode.kotlin.*
-import test.taylor.com.taylorcode.startActivity
 
-class GlideActivity : AppCompatActivity() {
+class GlideActivity2 : AppCompatActivity() {
 
     private lateinit var iv: ImageView
     private val contentView by lazy {
@@ -19,31 +16,27 @@ class GlideActivity : AppCompatActivity() {
             layout_width = match_parent
             layout_height = match_parent
 
-            iv = ImageView {
-                layout_id = "tvChange"
-                layout_width = 100
-                layout_height = 100
-                scaleType = scale_fit_xy
-                center_vertical = true
-                center_horizontal = true
-            }
-
             TextView {
                 layout_id = "tvChange"
                 layout_width = wrap_content
                 layout_height = wrap_content
                 textSize = 12f
                 textColor = "#ffffff"
-                text = "go to another activity"
+                text = "glide activity 2"
                 gravity = gravity_center
-                shape = shape {
-                    corner_radius = 50
-                    solid_color = "#ff00ff"
-                }
-                onClick = {
-                    startActivity<GlideActivity2>()
-                }
+                top_toTopOf = parent_id
+                center_horizontal = true
             }
+
+            iv = ImageView {
+                layout_id = "tvChange"
+                layout_width = 50
+                layout_height = 50
+                scaleType = scale_fit_xy
+                center_vertical = true
+                center_horizontal = true
+            }
+
 
         }
     }
@@ -51,7 +44,7 @@ class GlideActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(contentView)
-        Glide.with(applicationContext)
+        Glide.with(this)
             .load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F68c7e4ec9a8b3804d387012c8f2c2c69f9db95a42b695-Rxfr9z_fw658&refer=http%3A%2F%2Fhbimg.b0.upaiyun.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653817842&t=9fa35989a42b23f39f692e03aebe4672")
             .into(iv)
     }
---
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/GlideDiskModule.kt
@@ -0,0 +1,17 @@
+package test.taylor.com.taylorcode.photo
+
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.cache.ExternalPreferredCacheDiskCacheFactory
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+@GlideModule
+class GlideDiskModule : AppGlideModule() {
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setDefaultRequestOptions { RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.RESOURCE) }
+    }
+}
