8b589ff83d2c3c493e43c93cae19ea39b632a691
wisdomtl <l.tang@neoclub.cn>
2022-05-05 15:43:23 +0800

    Feat: okhttp3 for glide in kotlin version

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/okhttp/model_loader/OkHttpStreamFetcher.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/okhttp/model_loader/OkHttpStreamFetcher.kt
@@ -1,109 +1,89 @@
-package test.taylor.com.taylorcode.photo.okhttp.model_loader;
+package test.taylor.com.taylorcode.photo.okhttp.model_loader
 
-import android.util.Log;
-import androidx.annotation.NonNull;
-import com.bumptech.glide.Priority;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.HttpException;
-import com.bumptech.glide.load.data.DataFetcher;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.util.ContentLengthInputStream;
-import com.bumptech.glide.util.Preconditions;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-import okhttp3.Call;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
+import android.util.Log
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.HttpException
+import com.bumptech.glide.load.data.DataFetcher
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.util.ContentLengthInputStream
+import com.bumptech.glide.util.Preconditions
+import okhttp3.*
+import java.io.IOException
+import java.io.InputStream
 
-/** Fetches an {@link InputStream} using the okhttp library. */
-public class OkHttpStreamFetcher implements DataFetcher<InputStream>, okhttp3.Callback {
-    private static final String TAG = "OkHttpFetcher";
-    private final Call.Factory client;
-    private final GlideUrl url;
-    private InputStream stream;
-    private ResponseBody responseBody;
-    private DataCallback<? super InputStream> callback;
+/** Fetches an [InputStream] using the okhttp library.  */
+class OkHttpStreamFetcher(client: Call.Factory, url: GlideUrl) : DataFetcher<InputStream>, Callback {
+    private val client: Call.Factory = client
+    private val url: GlideUrl = url
+    private var stream: InputStream? = null
+    private var responseBody: ResponseBody? = null
+    private var callback: DataFetcher.DataCallback<in InputStream>? = null
+
     // call may be accessed on the main thread while the object is in use on other threads. All other
     // accesses to variables may occur on different threads, but only one at a time.
-    private volatile Call call;
-
-    // Public API.
-    @SuppressWarnings("WeakerAccess")
-    public OkHttpStreamFetcher(Call.Factory client, GlideUrl url) {
-        this.client = client;
-        this.url = url;
-    }
-
-    @Override
-    public void loadData(
-            @NonNull Priority priority, @NonNull final DataCallback<? super InputStream> callback) {
-        Request.Builder requestBuilder = new Request.Builder().url(url.toStringUrl());
-        for (Map.Entry<String, String> headerEntry : url.getHeaders().entrySet()) {
-            String key = headerEntry.getKey();
-            requestBuilder.addHeader(key, headerEntry.getValue());
+    @Volatile
+    private var call: Call? = null
+    override fun loadData(
+        priority: Priority, callback: DataFetcher.DataCallback<in InputStream>
+    ) {
+        val requestBuilder: Request.Builder = Request.Builder().url(url.toStringUrl())
+        for ((key, value) in url.headers) {
+            requestBuilder.addHeader(key, value)
         }
-        Request request = requestBuilder.build();
-        this.callback = callback;
-
-        call = client.newCall(request);
-        call.enqueue(this);
+        val request: Request = requestBuilder.build()
+        this.callback = callback
+        call = client.newCall(request)
+        call!!.enqueue(this)
     }
 
-    @Override
-    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+    override fun onFailure(call: Call, e: IOException) {
         if (Log.isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, "OkHttp failed to obtain result", e);
+            Log.d(TAG, "OkHttp failed to obtain result", e)
         }
-
-        callback.onLoadFailed(e);
+        callback!!.onLoadFailed(e)
     }
 
-    @Override
-    public void onResponse(@NonNull Call call, @NonNull Response response) {
-        responseBody = response.body();
-        if (response.isSuccessful()) {
-            long contentLength = Preconditions.checkNotNull(responseBody).contentLength();
-            stream = ContentLengthInputStream.obtain(responseBody.byteStream(), contentLength);
-            callback.onDataReady(stream);
+    override fun onResponse(call: Call, response: Response) {
+        responseBody = response.body
+        if (response.isSuccessful) {
+            val contentLength = Preconditions.checkNotNull(responseBody).contentLength()
+            stream = ContentLengthInputStream.obtain(responseBody!!.byteStream(), contentLength)
+            callback!!.onDataReady(stream)
         } else {
-            callback.onLoadFailed(new HttpException(response.message(), response.code()));
+            callback!!.onLoadFailed(HttpException(response.message, response.code))
         }
     }
 
-    @Override
-    public void cleanup() {
+    override fun cleanup() {
         try {
             if (stream != null) {
-                stream.close();
+                stream!!.close()
             }
-        } catch (IOException e) {
+        } catch (e: IOException) {
             // Ignored
         }
         if (responseBody != null) {
-            responseBody.close();
+            responseBody!!.close()
         }
-        callback = null;
+        callback = null
     }
 
-    @Override
-    public void cancel() {
-        Call local = call;
-        if (local != null) {
-            local.cancel();
-        }
+    override fun cancel() {
+        val local = call
+        local?.cancel()
     }
 
-    @NonNull
-    @Override
-    public Class<InputStream> getDataClass() {
-        return InputStream.class;
+    override fun getDataClass(): Class<InputStream> {
+        return InputStream::class.java
     }
 
-    @NonNull
-    @Override
-    public DataSource getDataSource() {
-        return DataSource.REMOTE;
+    override fun getDataSource(): DataSource {
+        return DataSource.REMOTE
     }
+
+    companion object {
+        private const val TAG = "OkHttpFetcher"
+    }
+
 }
--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/okhttp/model_loader/OkHttpUrlLoader.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/photo/okhttp/model_loader/OkHttpUrlLoader.kt
@@ -1,241 +1,62 @@
-package test.taylor.com.taylorcode.photo.okhttp.model_loader;
+package test.taylor.com.taylorcode.photo.okhttp.model_loader
 
-import android.util.Log;
+import com.bumptech.glide.load.Options
+import com.bumptech.glide.load.model.ModelLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.ModelLoader.LoadData
+import kotlin.jvm.JvmOverloads
+import com.bumptech.glide.load.model.ModelLoaderFactory
+import com.bumptech.glide.load.model.MultiModelLoaderFactory
+import okhttp3.OkHttpClient
+import kotlin.jvm.Volatile
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.InputStream
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.bumptech.glide.load.Options;
-import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.load.model.ModelLoader;
-import com.bumptech.glide.load.model.ModelLoaderFactory;
-import com.bumptech.glide.load.model.MultiModelLoaderFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.util.List;
-
-import okhttp3.Call;
-import okhttp3.Connection;
-import okhttp3.EventListener;
-import okhttp3.Handshake;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Protocol;
-import okhttp3.Request;
-import okhttp3.Response;
-
-/** A simple model loader for fetching media over http/https using OkHttp. */
-public class OkHttpUrlLoader implements ModelLoader<GlideUrl, InputStream> {
-
-    private final Call.Factory client;
-
-    // Public API.
-    @SuppressWarnings("WeakerAccess")
-    public OkHttpUrlLoader(@NonNull Call.Factory client) {
-        this.client = client;
+/** A simple model loader for fetching media over http/https using OkHttp.  */
+class OkHttpUrlLoader     // Public API.
+    (private val client: okhttp3.Call.Factory) : ModelLoader<GlideUrl, InputStream> {
+    override fun handles(url: GlideUrl): Boolean {
+        return true
     }
 
-    @Override
-    public boolean handles(@NonNull GlideUrl url) {
-        return true;
+    override fun buildLoadData(
+        model: GlideUrl, width: Int, height: Int, options: Options
+    ): LoadData<InputStream>? {
+        return LoadData(model, OkHttpStreamFetcher(client, model))
     }
 
-    @Override
-    public LoadData<InputStream> buildLoadData(
-            @NonNull GlideUrl model, int width, int height, @NonNull Options options) {
-        return new LoadData<>(model, new OkHttpStreamFetcher(client, model));
-    }
+    /** The default factory for [OkHttpUrlLoader]s.  */ // Public API.
+    class Factory
+    /** Constructor for a new Factory that runs requests using a static singleton client.  */ @JvmOverloads constructor(
+        private val client: okhttp3.Call.Factory = internalClient!!
+    ) : ModelLoaderFactory<GlideUrl, InputStream> {
+        override fun build(multiFactory: MultiModelLoaderFactory): ModelLoader<GlideUrl, InputStream> {
+            return OkHttpUrlLoader(client)
+        }
 
-    /** The default factory for {@link OkHttpUrlLoader}s. */
-    // Public API.
-    @SuppressWarnings("WeakerAccess")
-    public static class Factory implements ModelLoaderFactory<GlideUrl, InputStream> {
-        private static volatile Call.Factory internalClient;
-        private final Call.Factory client;
+        override fun teardown() {
+            // Do nothing, this instance doesn't own the client.
+        }
 
-        private static Call.Factory getInternalClient() {
-            if (internalClient == null) {
-                synchronized (Factory.class) {
-                    if (internalClient == null) {
-                        internalClient = (Call.Factory) new OkHttpClient.Builder().eventListener(new EventListener() {
-                            @Override
-                            public void cacheConditionalHit(@NonNull Call call, @NonNull Response cachedResponse) {
-                                super.cacheConditionalHit(call, cachedResponse);
+        companion object {
+            @Volatile
+            private var internalClient: okhttp3.Call.Factory? = null
+                private get() {
+                    if (field == null) {
+                        synchronized(Factory::class.java) {
+                            if (field == null) {
+                                field = OkHttpClient.Builder()
+                                    .addInterceptor(HttpLoggingInterceptor()).build()
                             }
-
-                            @Override
-                            public void cacheHit(@NonNull Call call, @NonNull Response response) {
-                                super.cacheHit(call, response);
-                            }
-
-                            @Override
-                            public void cacheMiss(@NonNull Call call) {
-                                super.cacheMiss(call);
-                            }
-
-                            @Override
-                            public void callEnd(@NonNull Call call) {
-                                super.callEnd(call);
-                            }
-
-                            @Override
-                            public void callFailed(@NonNull Call call, @NonNull IOException ioe) {
-                                super.callFailed(call, ioe);
-                            }
-
-                            @Override
-                            public void callStart(@NonNull Call call) {
-                                Log.v("ttaylor","[okhttp-glide]onCallStart()");
-                            }
-
-                            @Override
-                            public void canceled(@NonNull Call call) {
-                                super.canceled(call);
-                            }
-
-                            @Override
-                            public void connectEnd(@NonNull Call call, @NonNull InetSocketAddress inetSocketAddress, @NonNull Proxy proxy, @Nullable Protocol protocol) {
-                                super.connectEnd(call, inetSocketAddress, proxy, protocol);
-                            }
-
-                            @Override
-                            public void connectFailed(@NonNull Call call, @NonNull InetSocketAddress inetSocketAddress, @NonNull Proxy proxy, @Nullable Protocol protocol, @NonNull IOException ioe) {
-                                super.connectFailed(call, inetSocketAddress, proxy, protocol, ioe);
-                            }
-
-                            @Override
-                            public void connectStart(@NonNull Call call, @NonNull InetSocketAddress inetSocketAddress, @NonNull Proxy proxy) {
-                                super.connectStart(call, inetSocketAddress, proxy);
-                            }
-
-                            @Override
-                            public void connectionAcquired(@NonNull Call call, @NonNull Connection connection) {
-                                super.connectionAcquired(call, connection);
-                            }
-
-                            @Override
-                            public void connectionReleased(@NonNull Call call, @NonNull Connection connection) {
-                                super.connectionReleased(call, connection);
-                            }
-
-                            @Override
-                            public void dnsEnd(@NonNull Call call, @NonNull String domainName, @NonNull List<InetAddress> inetAddressList) {
-                                super.dnsEnd(call, domainName, inetAddressList);
-                            }
-
-                            @Override
-                            public void dnsStart(@NonNull Call call, @NonNull String domainName) {
-                                super.dnsStart(call, domainName);
-                            }
-
-                            @Override
-                            public void proxySelectEnd(@NonNull Call call, @NonNull HttpUrl url, @NonNull List<Proxy> proxies) {
-                                super.proxySelectEnd(call, url, proxies);
-                            }
-
-                            @Override
-                            public void proxySelectStart(@NonNull Call call, @NonNull HttpUrl url) {
-                                super.proxySelectStart(call, url);
-                            }
-
-                            @Override
-                            public void requestBodyEnd(@NonNull Call call, long byteCount) {
-                                super.requestBodyEnd(call, byteCount);
-                            }
-
-                            @Override
-                            public void requestBodyStart(@NonNull Call call) {
-                                super.requestBodyStart(call);
-                            }
-
-                            @Override
-                            public void requestFailed(@NonNull Call call, @NonNull IOException ioe) {
-                                super.requestFailed(call, ioe);
-                            }
-
-                            @Override
-                            public void requestHeadersEnd(@NonNull Call call, @NonNull Request request) {
-                                super.requestHeadersEnd(call, request);
-                            }
-
-                            @Override
-                            public void requestHeadersStart(@NonNull Call call) {
-                                super.requestHeadersStart(call);
-                            }
-
-                            @Override
-                            public void responseBodyEnd(@NonNull Call call, long byteCount) {
-                                super.responseBodyEnd(call, byteCount);
-                            }
-
-                            @Override
-                            public void responseBodyStart(@NonNull Call call) {
-                                super.responseBodyStart(call);
-                            }
-
-                            @Override
-                            public void responseFailed(@NonNull Call call, @NonNull IOException ioe) {
-                                super.responseFailed(call, ioe);
-                            }
-
-                            @Override
-                            public void responseHeadersEnd(@NonNull Call call, @NonNull Response response) {
-                                super.responseHeadersEnd(call, response);
-                            }
-
-                            @Override
-                            public void responseHeadersStart(@NonNull Call call) {
-                                super.responseHeadersStart(call);
-                            }
-
-                            @Override
-                            public void satisfactionFailure(@NonNull Call call, @NonNull Response response) {
-                                super.satisfactionFailure(call, response);
-                            }
-
-                            @Override
-                            public void secureConnectEnd(@NonNull Call call, @Nullable Handshake handshake) {
-                                super.secureConnectEnd(call, handshake);
-                            }
-
-                            @Override
-                            public void secureConnectStart(@NonNull Call call) {
-                                super.secureConnectStart(call);
-                            }
-                        }).build();
+                        }
                     }
+                    return field
                 }
-            }
-            return internalClient;
         }
-
-        /** Constructor for a new Factory that runs requests using a static singleton client. */
-        public Factory() {
-            this(getInternalClient());
-        }
-
         /**
          * Constructor for a new Factory that runs requests using given client.
          *
-         * @param client this is typically an instance of {@code OkHttpClient}.
+         * @param client this is typically an instance of `OkHttpClient`.
          */
-        public Factory(@NonNull Call.Factory client) {
-            this.client = client;
-        }
-
-        @NonNull
-        @Override
-        public ModelLoader<GlideUrl, InputStream> build(MultiModelLoaderFactory multiFactory) {
-            return new OkHttpUrlLoader(client);
-        }
-
-        @Override
-        public void teardown() {
-            // Do nothing, this instance doesn't own the client.
-        }
     }
 }
