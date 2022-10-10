de66e946cb5d7dac1c04d39797a4560f48a9fe6c
tangliang <tangliang@bilibili.com>
2022-10-10 16:50:18 +0800

    Feat: custom iterator which start from specific index and do round robin

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/fragment/FragmentActivity.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/ui/fragment/FragmentActivity.kt
@@ -41,9 +41,45 @@ class FragmentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(contentView)
 
-//        supportFragmentManager.beginTransaction().add("container".toLayoutId(), Fragment1()).commit()
-        DialogFragment1().show(supportFragmentManager,"ddd")
-        supportFragmentManager.setFragmentResultListener("abc",this) { requestKey, result -> Log.v("ttaylor","activity.onCreate() result=${result["111"]}") }
-        supportFragmentManager.setFragmentResultListener("efg",this) { requestKey, result -> Log.v("ttaylor","activity.onCreate() result=${result["112"]}") }
+        //        supportFragmentManager.beginTransaction().add("container".toLayoutId(), Fragment1()).commit()
+        DialogFragment1().show(supportFragmentManager, "ddd")
+        supportFragmentManager.setFragmentResultListener("abc", this) { requestKey, result -> Log.v("ttaylor", "activity.onCreate() result=${result["111"]}") }
+        supportFragmentManager.setFragmentResultListener("efg", this) { requestKey, result -> Log.v("ttaylor", "activity.onCreate() result=${result["112"]}") }
+
+        val iterator = listOf("a", "b", "c", "d").iterator(4)
+        while (iterator.hasNext()) {
+            Log.v("ttaylor", "onCreate()[iterator] nextItem=${iterator.next()}")
+        }
+        listOf("1","2","3").forEach {
+            if(it == "2") {
+                return@forEach
+            }
+            Log.v("ttaylor","foreach = ${it}")
+        }
+    }
+}
+
+/**
+ * case: custom iterator
+ */
+fun <T> List<T>.iterator(startIndex: Int): Iterator<IndexedValue<T>> = object : Iterator<IndexedValue<T>> {
+    private var index: Int = startIndex
+    private var bigger = true
+
+    init {
+        require(startIndex >= 0 && startIndex < this@iterator.size) { "Index out of bounds of List" }
+    }
+
+    override fun hasNext(): Boolean {
+        return if (bigger) index >= startIndex else index < startIndex
+    }
+
+    override fun next(): IndexedValue<T> {
+        val current = this@iterator[index]
+        return IndexedValue(index, current).also {
+            val nextIndex = index + 1
+            index = nextIndex.mod(this@iterator.size)
+            if (index == 0) bigger = false
+        }
     }
 }
