00c621545fbf015b11cf61c6a2e8bf315938c423
tangliang <tangliang@zenmen.com>
2023-03-15 20:19:43 +0800

    chore: invoke约定

--- TaylorCode/app/src/main/java/test/taylor/com/taylorcode/kotlin/Invoke2.kt
+++ TaylorCode/app/src/main/java/test/taylor/com/taylorcode/kotlin/Invoke2.kt
@@ -2,6 +2,9 @@ package test.taylor.com.taylorcode.kotlin
 
 import android.util.Log
 
+/**
+ * case:invoke 约定，类实现lambda并重写invoke函数
+ */
 class Invoke2:()->Int {
     override fun invoke(): Int {
         Log.v("ttaylor","tag=, Invoke2.invoke()  ")
